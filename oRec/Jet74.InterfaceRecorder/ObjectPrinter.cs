83efceb635ef9c373fecd1c675b473a26c7dc4e2
Tobbe1974 <tobbe.petersson@gmail.com>
2013-11-28 23:53:02 +0100

    Added little more to ObjectPrinter

--- oRec/Jet74.InterfaceRecorder/ObjectPrinter.cs
+++ oRec/Jet74.InterfaceRecorder/ObjectPrinter.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Castle.DynamicProxy;
 
@@ -12,6 +14,37 @@ namespace Jet74.InterfaceRecorder
         public void Intercept(IInvocation invocation)
         {
             invocation.Proceed();
+
+            PrintObjectTree(invocation);
+        }
+
+        private void PrintObjectTree(IInvocation invocation)
+        {
+            string fileName = CreateFilename(invocation);
+            
+        }
+
+        private string CreateFilename(IInvocation invocation)
+        {
+            var builder = new StringBuilder();
+
+            builder.Append(invocation.InvocationTarget.GetType().FullName);
+            builder.Append(".");
+            builder.Append(invocation.Method.Name);
+            builder.Append("(");
+
+            foreach (var arg in invocation.Arguments)
+            {
+                builder.Append(arg);
+            }
+            builder.Append(")");
+            return CleanIllegalChars(builder.ToString());
+        }
+
+        private static string CleanIllegalChars(string result)
+        {
+            //TODO : Implemet
+            return result;
         }
     }
 }
